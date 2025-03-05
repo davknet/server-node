7d0c1c20e96f00d7b3b0a6c6e1abd215c2f8da5a
david kahadze <davknet1@gmail.com>
2025-03-05 13:58:24 +0200

    has created router and handler

---
+++ Server/Routes.js
@@ -0,0 +1,47 @@
+/** 
+ * 
+ * Declare your routes here !!! 
+ * use var instead of const because
+ * i'm going to use try catch keywords 
+ * 
+ **/
+
+
+
+var handler = {};
+
+
+
+
+handler.api_base_user_data = function (data , calback ){
+
+    console.log( data.buffer ) ; 
+    const userData = data.buffer ;
+
+    calback( 200 ,  { "user" : userData }  ) ;
+
+
+};
+
+
+
+
+handler.notFound          = function( data , callback ){
+
+    callback( 404 , { "data" :"Page Not Found" }  );
+};
+
+
+
+
+var router  = {
+
+    'api_base_user_data' : handler.api_base_user_data ,
+} ;
+
+
+
+
+
+
+module.exports = { router  , handler } ;
