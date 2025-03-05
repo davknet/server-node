cada30a3d4c7b7a1cab57c422482a8a7f4a43dbc
david kahadze <davknet1@gmail.com>
2025-03-05 11:56:01 +0200

    The index.js and CustomException.js has been created

---
+++ Server/CustomException.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+export class CustomException extends Error{
+
+    constructor( message , statusCode ){
+
+         super(message);
+         this.name       = this.constructor.name ;
+         this.statusCode = statusCode || 500 ;
+         Error.captureStackTrace(this, this.constructor ) ;
+        
+    }
+
+
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Simple Node.js Server using ES6 Classes
+ * 
+ * Developed by: [Your Name]
+ * Date: [Today's Date]
+ * License: MIT
+ * 
+ * Description:
+ * This is a basic HTTP server built using ES6 classes. It listens on a specified port
+ * and responds with a simple message when accessed.
+ */
+
+
+
+
