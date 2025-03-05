077667fb4db68a80ea9da347388605ce345d3d38
david kahadze <davknet1@gmail.com>
2025-03-05 16:13:24 +0200

    A lot of bugs have been fixed, and the http server receives requests on port 3000

---
+++ Server/Serve.js
@@ -0,0 +1,15 @@
+
+/** 
+ *  Prefer to use var when declaring variables because 
+ *   I'm going to use try catch  keywords, so if you modify 
+ *  Please take it in mined 
+ * 
+ */
+
+var  http = require("http");
+var  url  = require("url");
+var  stringDecoder = require("string_decoder").StringDecoder;
+
+
+
+
