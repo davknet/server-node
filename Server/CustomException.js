6e781dd694a8ad3534a7b5e1e79a1dfea3e060a1
david kahadze <davknet1@gmail.com>
2025-03-14 13:13:07 +0200

    Modified CustomeException class

--- Server/CustomException.js
+++ Server/CustomException.js
@@ -17,8 +17,19 @@
          this.name       = this.constructor.name ;
          this.statusCode = statusCode || 500 ;
          Error.captureStackTrace(this, this.constructor ) ;
+
+         const stackLine = this.stack.split("\n")[1].trim(); 
+         const match     = stackLine.match(/\((.*):(\d+):(\d+)\)/);
+
+         if (match) {
+          this.fileName     = match[1];  
+          this.lineNumber   = match[2]; 
+          this.columnNumber = match[3];
+      }
         
     }
+
+    
 
 
 }
