2132caad1eeeab88ec97e592a2ecef0c69dfcecb
david kahadze <davknet1@gmail.com>
2025-03-14 13:39:16 +0200

    custome exception is ready

--- Server/Server.js
+++ Server/Server.js
@@ -8,12 +8,13 @@
 
 var  https          = require('https');
 var  UnifiedServer  = require('./UnifiedServer');
-var  http    = require("http");
+var  http    =    require("http");
 var  url     = require("url");
 var  config  = require("./Config");
 var  fs      = require('fs');
 var  decoder = require("string_decoder").StringDecoder;
 var  { router , handler } = require('./Routes');
+
 
 
             try{
@@ -26,9 +27,17 @@ var  { router , handler } = require('./Routes');
 
             }catch(e){
     
-                console.log()
+                console.error( `Error name : ${e.name} `); 
                 console.error("an error occurred :", e.message); 
+                console.error( "status : " ,e.statusCode);
+                console.error( "in the file :" , e.fileName); 
+                console.error("at line : " , e.lineNumber);
+                console.error("column number : " ,e.columnNumber);
                 console.error(e.stack);
+
+                  
+ 
+   
     
             }finally{
     
